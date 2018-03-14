ae9085e94f7268964fc6898fdfcceee69842f3f8
Shyam Deshmukh <shyamdeshmukh1@gmail.com>
2018-03-14 15:29:42 +0530

    Add & display Events

--- pawana_camping/Controllers/HomeController.cs
+++ pawana_camping/Controllers/HomeController.cs
@@ -3,13 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using pawana_camping.Models;
+using System.Windows.Forms;
 
 namespace pawana_camping.Controllers
 {
     public class HomeController : Controller
     {
+        private int offset = 0; 
+
         public ActionResult Index()
         {
+            HttpContext.Session.Add("offset", 0);
+
+            var obj = new db_connect();
+            List<string>[] list = new List<string>[3];
+            MessageBox.Show(offset.ToString());
+            list = obj.events_show(offset);
+            ViewBag.list = list;
+            ViewBag.total = list[0].Count();
+
             return View();
         }
 
@@ -37,5 +50,187 @@ namespace pawana_camping.Controllers
         {
             return View();
         }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult login_btn(string username, string password)
+        {
+            try
+            {
+                var obj = new db_connect();
+
+                if (obj.Login(username, password))
+                {
+                    TempData["AlertMessage"] = "Logged in successfully";
+                    HttpContext.Session.Add("user", 1);
+                    return RedirectToAction("Eventfeed", "Home");
+
+                }
+                else
+                {
+                    TempData["AlertMessage"] = "Your username or password is not correct";
+                    HttpContext.Session.Add("user", 0);
+                    return RedirectToAction("Login", "Home");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                HttpContext.Session.Add("user", 0);
+                System.Web.HttpContext.Current.Response.Write("<script>alert('There is some issue while saving the details, please try again, Thanks.')</script>");
+                return RedirectToAction("Login", "Home");
+            }
+        }
+
+        public ActionResult Eventfeed()
+        {
+            try
+            {
+                string session_status = HttpContext.Session["user"].ToString();
+                if (Int32.Parse(session_status) == 0)
+                    return RedirectToAction("Login", "Home");
+                else
+                    return View();
+            }
+            catch (Exception ex)
+            {
+                HttpContext.Session.Add("user", 0);
+                System.Web.HttpContext.Current.Response.Write("<script>alert('There is some issue while saving the details, please try again, Thanks.')</script>");
+                return RedirectToAction("Login", "Home");
+            }
+        }
+
+        public ActionResult submit_event_btn(string heading, string description)
+        {
+            try
+            {
+                var obj = new db_connect();
+                obj.Insert_Event(heading, description);
+                return RedirectToAction("Eventfeed", "Home");
+            }
+            catch (Exception ex)
+            {
+                System.Web.HttpContext.Current.Response.Write("<script>alert('There is some issue while saving the details, please try again, Thanks.')</script>");
+                return RedirectToAction("Eventfeed", "Home");
+            }
+        }
+
+        public ActionResult logout_btn(string heading, string description)
+        {
+            try
+            {
+                HttpContext.Session.Add("user", 0);
+                System.Web.HttpContext.Current.Response.Write("<script>alert('There is some issue while saving the details, please try again, Thanks.')</script>");
+                return RedirectToAction("Login", "Home");
+            }
+            catch (Exception ex)
+            {
+                System.Web.HttpContext.Current.Response.Write("<script>alert('There is some issue while saving the details, please try again, Thanks.')</script>");
+                return RedirectToAction("Login", "Home");
+            }
+        }
+
+        public ActionResult first_btn()
+        {
+            try
+            {
+                var obj = new db_connect();
+                List<string>[] list = new List<string>[3];
+                list = obj.events_show(0);
+                ViewBag.list = list;
+                ViewBag.total = list[0].Count();
+
+                return RedirectToAction("Index", "Home");
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+        public ActionResult previous_btn()
+        {
+            try
+            {
+                if (offset <= 1)
+                {
+                    offset = 0;
+                }
+                else
+                {
+                    offset = offset - 2;
+                }
+
+                var obj = new db_connect();
+                List<string>[] list = new List<string>[3];
+                list = obj.events_show(offset);
+                ViewBag.list = list;
+                ViewBag.total = list[0].Count();
+
+                return RedirectToAction("Index", "Home");
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+        public ActionResult next_btn()
+        {
+            try
+            {   
+                var obj = new db_connect();
+                int cnt = obj.event_count();
+                offset = offset + 2;
+                if (offset > cnt)
+                {
+                    offset = cnt-2;
+                }
+                offset = 2;
+                List<string>[] list = new List<string>[3];
+                list = obj.events_show(1);
+                ViewBag.list = list;
+                ViewBag.total = list[0].Count();
+                return PartialView("event_show");
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+        public ActionResult last_btn()
+        {
+            try
+            {
+                offset = offset + 2;
+                var obj = new db_connect();
+                int cnt = obj.event_count();
+                if (cnt != 0)
+                {
+                    if (cnt > 0)
+                    {
+                        if (cnt % 2 == 0)
+                            offset = cnt - 2;
+                        else
+                            offset = cnt - 1;
+                    }
+                }
+                List<string>[] list = new List<string>[3];
+                list = obj.events_show(offset);
+                ViewBag.list = list;
+                ViewBag.total = list[0].Count();
+
+                return RedirectToAction("Index", "Home");
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
     }
 }
---
+++ pawana_camping/Models/db_connect.cs
@@ -0,0 +1,301 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+
+namespace pawana_camping.Models
+{
+    public class db_connect
+    {
+        private MySqlConnection connection;
+        public List<string>[] list_feedback_show = new List<string>[3];
+        public List<string>[] list_time_show = new List<string>[1];
+        public List<string>[] list_gallery_show = new List<string>[2];
+
+        public List<string>[] list_events_show = new List<string>[3];
+
+        private bool OpenConnection()
+        {
+            string connetionString = null;
+            connetionString = "server=182.50.133.77;database=pawna_camping;uid=pawnaadmin;pwd=Pawnaadmin@123;Allow User Variables=True;";
+            connection = new MySqlConnection(connetionString);
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+
+                return false;
+            }
+        }
+
+        private bool CloseConnection()
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                return false;
+            }
+        }
+
+        public int Insert(string name, string email, string comment_type, string sub, string msg)
+        {
+            try
+            {
+                int id = -1;
+                string query = "INSERT INTO testimony (Name, Email_id, Comment_type, Subject, Message, Status, Date) VALUES(@name, @email, @com_type, @sub, @msg, @sts, NOW())";
+
+                if (this.OpenConnection() == true)
+                {
+                    MySqlCommand cmd = new MySqlCommand(query, connection);
+                    cmd.Parameters.AddWithValue("@name", name);
+                    cmd.Parameters.AddWithValue("@email", email);
+                    cmd.Parameters.AddWithValue("@com_type", comment_type);
+                    cmd.Parameters.AddWithValue("@sub", sub);
+                    cmd.Parameters.AddWithValue("@msg", msg);
+                    cmd.Parameters.AddWithValue("@sts", false);
+
+                    cmd.ExecuteNonQuery();
+
+                    MySqlCommand cmd1 = new MySqlCommand("SELECT LAST_INSERT_ID()", connection);
+                    id = Convert.ToInt32(cmd1.ExecuteScalar());
+
+                    this.CloseConnection();
+                }
+                return id;
+            }
+            catch (MySqlException ex)
+            {
+                return -1;
+            }
+        }
+
+        public int Insert_Booking(string name_booking, string email_booking, string phone, string start_time, string session_type, string date)
+        {
+            try
+            {
+                string query = "INSERT INTO booking (Name, Email, Phone, Start_time, Session_type, Date) VALUES(@name, @email, @phone, @time, @session, @dt)";
+
+                if (this.OpenConnection() == true)
+                {
+                    MySqlCommand cmd = new MySqlCommand(query, connection);
+                    cmd.Parameters.AddWithValue("@name", name_booking);
+                    cmd.Parameters.AddWithValue("@email", email_booking);
+                    cmd.Parameters.AddWithValue("@phone", phone);
+                    cmd.Parameters.AddWithValue("@time", start_time);
+                    cmd.Parameters.AddWithValue("@session", session_type);
+                    cmd.Parameters.AddWithValue("@dt", date);
+                    cmd.ExecuteNonQuery();
+
+                    this.CloseConnection();
+                }
+                return 0;
+            }
+            catch (MySqlException ex)
+            {
+                return ex.Number;
+            }
+        }
+
+        public List<string>[] testimony_show(int offset)
+        {
+            try
+            {
+                //string query = "SELECT * FROM testimony ORDER BY Date DESC, ID DESC LIMIT 2 OFFSET @offset";
+                string query = "SELECT * FROM testimony where Comment_type='Testimony'";
+
+                list_feedback_show[0] = new List<string>();
+                list_feedback_show[1] = new List<string>();
+                list_feedback_show[2] = new List<string>();
+
+                if (this.OpenConnection() == true)
+                {
+                    MySqlCommand cmd = new MySqlCommand(query, connection);
+                    cmd.Parameters.AddWithValue("@offset", offset);
+                    MySqlDataReader dataReader = cmd.ExecuteReader();
+
+                    while (dataReader.Read())
+                    {
+                        list_feedback_show[0].Add(dataReader["Subject"] + "");
+                        list_feedback_show[1].Add(dataReader["Message"] + "");
+                        list_feedback_show[2].Add(dataReader["Date"] + "");
+                    }
+
+                    dataReader.Close();
+                    this.CloseConnection();
+                    MessageBox.Show(list_feedback_show[0].ToString());
+                    return list_feedback_show;
+                }
+                else
+                {
+                    return list_feedback_show;
+                }
+            }
+            catch (MySqlException ex)
+            {
+                return list_feedback_show;
+            }
+        }
+
+        public List<string>[] time_slot_show(string date)
+        {
+            try
+            {
+                string query = "SELECT start_time FROM booking where date = @date";
+
+                list_time_show[0] = new List<string>();
+                //list_feedback_show[1] = new List<string>();
+                //list_feedback_show[2] = new List<string>();
+
+                if (this.OpenConnection() == true)
+                {
+                    MySqlCommand cmd = new MySqlCommand(query, connection);
+                    cmd.Parameters.AddWithValue("@date", date);
+                    MySqlDataReader dataReader = cmd.ExecuteReader();
+
+                    while (dataReader.Read())
+                    {
+                        list_time_show[0].Add(dataReader["start_time"] + "");
+                    }
+
+                    dataReader.Close();
+                    this.CloseConnection();
+                    return list_time_show;
+                }
+                else
+                {
+                    return list_time_show;
+                }
+            }
+            catch (MySqlException ex)
+            {
+                return list_time_show;
+            }
+        }
+
+        /*News Section */
+        public List<string>[] events_show(int offset)
+        {
+            try
+            {
+                string query = "SELECT * FROM events ORDER BY ID DESC LIMIT 2 OFFSET @offset";
+
+                list_events_show[0] = new List<string>();
+                list_events_show[1] = new List<string>();
+                list_events_show[2] = new List<string>();
+
+
+                if (this.OpenConnection() == true)
+                {
+                    MySqlCommand cmd = new MySqlCommand(query, connection);
+                    cmd.Parameters.AddWithValue("@offset", offset);
+                    MySqlDataReader dataReader = cmd.ExecuteReader();
+
+                    while (dataReader.Read())
+                    {
+                        list_events_show[0].Add(dataReader["Heading"] + "");
+                        list_events_show[1].Add(dataReader["Description"] + "");
+                        list_events_show[2].Add(dataReader["Date"] + "");
+                    }
+
+                    dataReader.Close();
+                    this.CloseConnection();
+                    return list_events_show;
+                }
+                else
+                {
+                    return list_events_show;
+                }
+            }
+            catch (MySqlException ex)
+            {
+                return list_events_show;
+            }
+        }
+
+        public int event_count()
+        {
+            try
+            {
+                int count = 0;
+                string query = "select count(id) from pawna_camping.events";
+                if (this.OpenConnection() == true)
+                {
+                    MySqlCommand cmd = new MySqlCommand(query, connection);
+                    count = Convert.ToInt32(cmd.ExecuteScalar());
+                    this.CloseConnection();
+                }
+                return count;
+            }
+            catch (MySqlException ex)
+            {
+                return 0;
+            }
+        }
+
+        public int Insert_Event(string heading, string description)
+        {
+            try
+            {
+                int id = -1;
+                string query = "INSERT INTO events (Heading, Description, Date) VALUES(@heading, @description, NOW())";
+
+                if (this.OpenConnection() == true)
+                {
+                    MySqlCommand cmd = new MySqlCommand(query, connection);
+                    cmd.Parameters.AddWithValue("@heading", heading);
+                    cmd.Parameters.AddWithValue("@description", description);
+
+                    cmd.ExecuteNonQuery();
+
+                    this.CloseConnection();
+                }
+                return id;
+            }
+            catch (MySqlException ex)
+            {
+                return -1;
+            }
+        }
+        /*News Section*/
+
+        /*Login Section*/
+        public Boolean Login(string name, string password)
+        {
+            try
+            {
+                MySqlDataReader rdr;
+                string query = "select * from login where username = @name and password = @password";
+
+                if (this.OpenConnection() == true)
+                {
+                    MySqlCommand cmd = new MySqlCommand(query, connection);
+                    cmd.Parameters.AddWithValue("@name", name);
+                    cmd.Parameters.AddWithValue("@password", password);
+                    rdr = cmd.ExecuteReader();
+                    if (rdr.Read())
+                    {
+                        this.CloseConnection();
+                        return true;
+                    }
+                }
+                this.CloseConnection();
+                return false;
+            }
+            catch (MySqlException ex)
+            {
+                return false;
+            }
+        }
+        /*Login Section*/
+
+    } //db_connect class
+} // namespace
