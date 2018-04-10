4f6d1e012b7afd84fe4a3a0f65f5dd27c485703d
Shyam Deshmukh <shyamdeshmukh1@gmail.com>
2018-04-10 23:43:13 +0530

    Payment status working on live site

--- pawana_camping/Controllers/HomeController.cs
+++ pawana_camping/Controllers/HomeController.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using pawana_camping.Models;
-using System.Windows.Forms;
 using System.Configuration;
 using System.Text;
 using System.Net.Mail;
@@ -24,11 +23,11 @@ namespace pawana_camping.Controllers
         public int event_page_size = 10;
         public int feedback_page_size = 3;
         public int booking_page_size = 10;
-
         
         public static string mailServer = "relay-hosting.secureserver.net";
         public static string mailFrom = "info@pawnaheritagecamping.com";
         public static string mailTo = "info@pawnaheritagecamping.com";
+        public static string mailCc = "dharma9191@gmail.com";
         public static string mailPassword = "Pawna@123";
         /*
         public static string mailServer = "smtp.gmail.com";
@@ -42,17 +41,11 @@ namespace pawana_camping.Controllers
         public ActionResult Index()
         {
             HttpContext.Session.Add("offset_event", 0);
-            //var obj = new db_connect();
             List<string>[] list = new List<string>[3];
             list = obj.events_show(Int32.Parse(HttpContext.Session["offset_event"].ToString()), 3);
             ViewBag.list = list;
             ViewBag.total = list[0].Count();
 
-            return View();
-        }
-
-        public ActionResult About()
-        {
             return View();
         }
 
@@ -64,7 +57,6 @@ namespace pawana_camping.Controllers
         [Authorize]
         public ActionResult Dashboard()
         {
-            //var obj = new db_connect();
             List<string>[] list = new List<string>[14];
             list = obj.bookings_show(0, booking_page_size);
             ViewBag.list = list;            
@@ -82,17 +74,11 @@ namespace pawana_camping.Controllers
         {
             HttpContext.Session.Add("offset_event", 0);
 
-            //var obj = new db_connect();
             List<string>[] list = new List<string>[3];
             list = obj.events_show(Int32.Parse(HttpContext.Session["offset_event"].ToString()), event_page_size);
             ViewBag.list = list;
             ViewBag.total = list[0].Count();
 
-            return View();
-        }
-
-        public ActionResult Blog()
-        {
             return View();
         }
 
@@ -105,7 +91,6 @@ namespace pawana_camping.Controllers
         {
             HttpContext.Session.Add("offset_feedback", 0);
 
-            //var obj = new db_connect();
             List<string>[] list = new List<string>[3];
             list = obj.feedback_show(Int32.Parse(HttpContext.Session["offset_feedback"].ToString()), feedback_page_size);
             ViewBag.list = list;
@@ -152,7 +137,8 @@ namespace pawana_camping.Controllers
             return RedirectToAction("Login", "Home");
         }
 
-        public ActionResult PaymentSuccess()
+        [HttpPost]
+        public ActionResult PaymentStatus(FormCollection form)
         {
             try
             {
@@ -162,109 +148,7 @@ namespace pawana_camping.Controllers
                 string order_id = string.Empty;
                 string hash_seq = "key|txnid|amount|productinfo|firstname|email|udf1|udf2|udf3|udf4|udf5|udf6|udf7|udf8|udf9|udf10";
 
-                if (Request.Form["status"] == "success")
-                {
-                    merc_hash_vars_seq = hash_seq.Split('|');
-                    Array.Reverse(merc_hash_vars_seq);
-                    merc_hash_string = ConfigurationManager.AppSettings["SALT"] + "|" + Request.Form["status"];
-
-                    foreach (string merc_hash_var in merc_hash_vars_seq)
-                    {
-                        merc_hash_string += "|";
-                        merc_hash_string = merc_hash_string + (Request.Form[merc_hash_var] != null ? Request.Form[merc_hash_var] : "");
-                    }
-                    //Response.Write(merc_hash_string); //exit;
-                    merc_hash = Generatehash512(merc_hash_string).ToLower();
-                    
-                    if (merc_hash != Request.Form["hash"])
-                    {
-                        //Response.Write("Hash value did not matched");
-                        order_id = Request.Form["txnid"];
-                    }
-                    else
-                    {
-                        order_id = Request.Form["txnid"];
-                    }
-
-                    ViewBag.name = Request.Form["firstname"];
-                    ViewBag.status = Request.Form["status"];
-                    ViewBag.email = Request.Form["email"];
-                    ViewBag.phone = Request.Form["phone"];
-                    ViewBag.tid = Request.Form["txnid"];
-                    ViewBag.tr_amt = Request.Form["amount"];
-                    ViewBag.tr_date_time = Request.Form["udf1"];
-                    ViewBag.bk_date_time = Request.Form["udf2"];
-                    ViewBag.adults = Request.Form["udf3"];
-                    ViewBag.children = Request.Form["udf4"];
-                    ViewBag.prod_info = Request.Form["productinfo"];
-                    ViewBag.part_payment = Request.Form["udf5"];
-
-                    //var obj = new db_connect();
-                    obj.Insert_Booking(ViewBag.tid, ViewBag.status, ViewBag.tr_date_time, ViewBag.prod_info,
-                    ViewBag.name, ViewBag.email, ViewBag.phone, ViewBag.bk_date_time, Int32.Parse(ViewBag.adults), Int32.Parse(ViewBag.children),
-                                      Int32.Parse(ViewBag.part_payment), (int)(Convert.ToDouble(ViewBag.tr_amt)));
-
-                    MailMessage mail = new MailMessage();
-                    SmtpClient SmtpServer = new SmtpClient(mailServer);
-
-                    mail.From = new MailAddress(mailFrom);
-                    mail.To.Add(mailFrom);
-                    mail.Subject = "Booking Details For : " + ViewBag.name;
-                    mail.IsBodyHtml = true;
-
-                    string htmlBody;
-
-                    htmlBody = "<html> <head>  </head> <body>" +
-                                "<div><img src=\"cid:icon_01\"> </div>" +
-                                "<table border=\"1\" style=\"font - family:Georgia, Garamond, Serif; width: 100 %; color: blue; font - style:italic; \"> <tr bgcolor=\"#00FFFF\" align=\"center\"> <th> Transaction Id </th> <th> Status </th> <th> Name </th> <th> Email </th> <th> Phone </th>  <th>Booking Date</th>  " +
-                                "<th>Adults</th> <th>Children</th> <th>Part Payment</th> <th>Paid Amount</th></tr> <tr align=\"center\"> " +
-                                "<td>" + ViewBag.tid + "</td>" +
-                                "<td>" + ViewBag.status + "</td>" +
-                                "<td>" + ViewBag.name + "</td>" +
-                                "<td>" + ViewBag.email + "</td>" +
-                                "<td>" + ViewBag.phone + "</td>" +
-                                "<td>" + ViewBag.bk_date_time + "</td>" +
-                                "<td>" + ViewBag.adults + "</td>" +
-                                "<td>" + ViewBag.children + "</td>" +
-                                "<td>" + ViewBag.part_payment + "</td>" +
-                                "<td>" + ViewBag.tr_amt + "</td>" +
-                                "</tr> </table> </body> </html> ";
-
-
-                    mail.Body = htmlBody;
-                    //SmtpServer.Port = 587;
-                    SmtpServer.Credentials = new System.Net.NetworkCredential(mailFrom, mailPassword);
-                    //SmtpServer.EnableSsl = true;
-
-                    SmtpServer.Send(mail);
-                    TempData["AlertMessage"] = "Your details saved successfully, We will get back to you shortly.";
-                    return RedirectToAction("Contact", "Home");
-                }
-                else
-                {
-                    //Response.Write("Hash value did not matched");
-                    // osc_redirect(osc_href_link(FILENAME_CHECKOUT, 'payment' , 'SSL', null, null,true));
-                }
-                                
-                return View();
-            }
-            catch (Exception ex)
-            {
-                Response.Write("<span style='color:red'>" + ex.Message + "</span>");
-                return View();
-            }
-        }
-
-        public ActionResult PaymentFailure()
-        {
-            try
-            {
-                string[] merc_hash_vars_seq;
-                string merc_hash_string = string.Empty;
-                string merc_hash = string.Empty;
-                string order_id = string.Empty;
-                string hash_seq = "key|txnid|amount|productinfo|firstname|email|udf1|udf2|udf3|udf4|udf5|udf6|udf7|udf8|udf9|udf10";
-
+                
                 merc_hash_vars_seq = hash_seq.Split('|');
                 Array.Reverse(merc_hash_vars_seq);
                 merc_hash_string = ConfigurationManager.AppSettings["SALT"] + "|" + Request.Form["status"];
@@ -300,11 +184,46 @@ namespace pawana_camping.Controllers
                 ViewBag.prod_info = Request.Form["productinfo"];
                 ViewBag.part_payment = Request.Form["udf5"];
 
-                //var obj = new db_connect();
                 obj.Insert_Booking(ViewBag.tid, ViewBag.status, ViewBag.tr_date_time, ViewBag.prod_info,
                 ViewBag.name, ViewBag.email, ViewBag.phone, ViewBag.bk_date_time, Int32.Parse(ViewBag.adults), Int32.Parse(ViewBag.children),
                                     Int32.Parse(ViewBag.part_payment), (int)(Convert.ToDouble(ViewBag.tr_amt)));
-            
+                
+                MailMessage mail = new MailMessage();
+                SmtpClient SmtpServer = new SmtpClient(mailServer);
+
+                mail.From = new MailAddress(mailFrom);
+                mail.To.Add(mailCc);
+                mail.CC.Add(mailTo);
+                mail.Bcc.Add(ViewBag.email);
+                mail.Subject = "Booking Details For : " + ViewBag.name;
+                mail.IsBodyHtml = true;
+
+                string htmlBody;
+
+                htmlBody = "<html> <head>  </head> <body>" +
+                            "<div><img src=\"cid:icon_01\"> </div>" +
+                            "<table border=\"1\" style=\"font - family:Georgia, Garamond, Serif; width: 100 %; color: blue; font - style:italic; \"> <tr bgcolor=\"#00FFFF\" align=\"center\"> <th> Transaction Id </th> <th> Status </th> <th> Name </th> <th> Email </th> <th> Phone </th>  <th>Booking Date</th>  " +
+                            "<th>Adults</th> <th>Children</th> <th>Part Payment</th> <th>Paid Amount</th></tr> <tr align=\"center\"> " +
+                            "<td>" + ViewBag.tid + "</td>" +
+                            "<td>" + ViewBag.status + "</td>" +
+                            "<td>" + ViewBag.name + "</td>" +
+                            "<td>" + ViewBag.email + "</td>" +
+                            "<td>" + ViewBag.phone + "</td>" +
+                            "<td>" + ViewBag.bk_date_time + "</td>" +
+                            "<td>" + ViewBag.adults + "</td>" +
+                            "<td>" + ViewBag.children + "</td>" +
+                            "<td>" + ViewBag.part_payment + "</td>" +
+                            "<td>" + ViewBag.tr_amt + "</td>" +
+                            "</tr> </table> </body> </html> ";
+
+
+                mail.Body = htmlBody;
+                //SmtpServer.Port = 587;
+                SmtpServer.Credentials = new System.Net.NetworkCredential(mailFrom, mailPassword);
+                //SmtpServer.EnableSsl = true;
+
+                SmtpServer.Send(mail);
+                               
                 return View();
             }
             catch (Exception ex)
@@ -312,13 +231,12 @@ namespace pawana_camping.Controllers
                 Response.Write("<span style='color:red'>" + ex.Message + "</span>");
                 return View();
             }
-        }
+        }        
         
         public ActionResult UpdateNow(string base_adult, string base_child)
         {
             try
             {
-                //var obj = new db_connect();
                 obj.update_rates(base_adult, base_child);
                 return RedirectToAction("ChangeRate", "Home");
             }
@@ -333,7 +251,6 @@ namespace pawana_camping.Controllers
         {
             try
             {
-                //var obj = new db_connect();
                 obj.Insert_Event(heading, description);
                 return RedirectToAction("Eventfeed", "Home");
             }
@@ -348,7 +265,6 @@ namespace pawana_camping.Controllers
         {
             try
             {
-                //var obj = new db_connect();
                 obj.insert_feedback(name, email, phone, subject, message);
                 return RedirectToAction("Feedback", "Home");
             }
@@ -363,7 +279,6 @@ namespace pawana_camping.Controllers
         {
             try
             {
-                //var obj = new db_connect();
                 List<string>[] list = new List<string>[3];
                 list = obj.events_show(0, event_page_size);
                 ViewBag.list = list;
@@ -387,7 +302,6 @@ namespace pawana_camping.Controllers
                     HttpContext.Session.Add("offset_event", 0);
                 }
 
-                //var obj = new db_connect();
                 List<string>[] list = new List<string>[3];
                 list = obj.events_show(Int32.Parse(HttpContext.Session["offset_event"].ToString()), event_page_size);
                 ViewBag.list = list;
@@ -405,7 +319,6 @@ namespace pawana_camping.Controllers
         {
             try
             {
-                //var obj = new db_connect();
                 int cnt = obj.event_count();
                 HttpContext.Session.Add("offset_event", (Int32.Parse(HttpContext.Session["offset_event"].ToString()) + event_page_size));
                 if (Int32.Parse(HttpContext.Session["offset_event"].ToString()) > cnt)
@@ -429,7 +342,6 @@ namespace pawana_camping.Controllers
         {
             try
             {
-                //var obj = new db_connect();
                 int cnt = obj.event_count();
                 if (cnt > 0)
                 {
@@ -460,7 +372,6 @@ namespace pawana_camping.Controllers
         {
             try
             {
-                //var obj = new db_connect();
                 List<string>[] list = new List<string>[3];
                 list = obj.feedback_show(0, feedback_page_size);
                 ViewBag.list = list;
@@ -484,7 +395,6 @@ namespace pawana_camping.Controllers
                     HttpContext.Session.Add("offset_feedback", 0);
                 }
 
-                //var obj = new db_connect();
                 List<string>[] list = new List<string>[3];
                 list = obj.feedback_show(Int32.Parse(HttpContext.Session["offset_feedback"].ToString()), feedback_page_size);
                 ViewBag.list = list;
@@ -502,7 +412,6 @@ namespace pawana_camping.Controllers
         {
             try
             {
-                //var obj = new db_connect();
                 int cnt = obj.feedback_count();
                 HttpContext.Session.Add("offset_feedback", (Int32.Parse(HttpContext.Session["offset_feedback"].ToString()) + feedback_page_size));
                 if (Int32.Parse(HttpContext.Session["offset_feedback"].ToString()) > cnt)
@@ -526,7 +435,6 @@ namespace pawana_camping.Controllers
         {
             try
             {
-                //var obj = new db_connect();
                 int cnt = obj.feedback_count();
                 if (cnt > 0)
                 {
@@ -557,7 +465,6 @@ namespace pawana_camping.Controllers
         {
             try
             {
-                //var obj = new db_connect();
                 List<string>[] list = new List<string>[14];
                 list = obj.bookings_show(0, booking_page_size);
                 ViewBag.list = list;
@@ -581,7 +488,6 @@ namespace pawana_camping.Controllers
                     HttpContext.Session.Add("offset_booking", 0);
                 }
 
-                //var obj = new db_connect();
                 List<string>[] list = new List<string>[14];
                 list = obj.bookings_show(Int32.Parse(HttpContext.Session["offset_booking"].ToString()), booking_page_size);
                 ViewBag.list = list;
@@ -599,7 +505,6 @@ namespace pawana_camping.Controllers
         {
             try
             {
-                //var obj = new db_connect();
                 int cnt = obj.booking_count();
                 HttpContext.Session.Add("offset_booking", (Int32.Parse(HttpContext.Session["offset_booking"].ToString()) + booking_page_size));
                 if (Int32.Parse(HttpContext.Session["offset_booking"].ToString()) > cnt)
@@ -623,7 +528,6 @@ namespace pawana_camping.Controllers
         {
             try
             {
-                //var obj = new db_connect();
                 int cnt = obj.booking_count();
                 if (cnt > 0)
                 {
@@ -653,9 +557,9 @@ namespace pawana_camping.Controllers
         public string action1 = string.Empty;
         public string hash1 = string.Empty;
         public string txnid1 = string.Empty;
-        //private object exit;
-
-        public void PayNow(string name, string email, string phone, string total_cost, string bk_date, string adult, string child, string partial_payment)
+        
+        [HttpPost]
+        public void Index(string name, string email, string phone, string total_cost, string bk_date, string adult, string child, string partial_payment)
         {
             try
             {
@@ -749,8 +653,8 @@ namespace pawana_camping.Controllers
                     data.Add("email", email);
                     data.Add("phone", phone);
                     data.Add("productinfo", "Tent Booking");
-                    data.Add("surl", "http://www.pawnaheritagecamping.com/Home/PaymentSuccess");
-                    data.Add("furl", "http://www.pawnaheritagecamping.com/Home/PaymentFailure");
+                    data.Add("surl", "http://pawnaheritagecamping.com/Home/PaymentStatus");
+                    data.Add("furl", "http://pawnaheritagecamping.com/Home/PaymentStatus");
                     data.Add("lastname", "thakar");
                     data.Add("curl", "http://www.pawnaheritagecamping.com");
                     data.Add("address1", "pune");
